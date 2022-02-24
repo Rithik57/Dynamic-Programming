2e8cc383c725499bd19bf9156688f63eab5c788e
RIthik Chaudhary <71806874+Rithik57@users.noreply.github.com>
2022-02-24 14:16:43 +0530

    Rod Cutting | Unbounded Knapsack

--- rodCutting.java
+++ rodCutting.java
@@ -1,28 +1,18 @@
-public class solution{
-    public static void main(String[] args) {
-        int[] length = new int[]{1,2,3,4,5,6,7,8};
-        int[] value = new int[]{1, 5, 8, 9, 10, 17, 17, 20};
-        System.out.println(rodCuttingDP(length,value,length.length,length.length));
+class Solution{
+    public int cutRod(int price[], int n) {
+        return solveDP(price,n,n);
     }
-    public static int rodCutting(int[] length,int[] value,int n,int cap){
-        if(n==0)return 0;
-        if(cap==0)return 0;
-        if(length[n-1] <= cap){
-            return Math.max(value[n-1] + rodCutting(length,value,n,cap-length[n-1]),rodCutting(length,value,n-1,cap));
-        }
-        return rodCutting(length,value,n-1,cap);
-    }
-    public static int rodCuttingDP(int[] length,int[] value,int n,int cap){
-        int[][] dp = new int[n+1][cap+1];
-        for(int i=1;i<n+1;i++){ // n by i
-            for(int j=1;j<cap+1;j++){ // j by cap
-                if(length[i-1] <= j){
-                    dp[i][j] = Math.max(value[i-1]+dp[i][j-length[i-1]],dp[i-1][j]);
+    public static int solveDP(int[] price,int W,int n){
+        int[][] dp = new int[W+1][n+1];
+        for(int i=1;i<W+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(j<=i){
+                    dp[i][j] = Math.max(price[j-1] + dp[i-j][j],dp[i][j-1]);
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i][j-1];
                 }
             }
         }
-        return dp[n][cap];
+        return dp[W][n];
     }
 }
